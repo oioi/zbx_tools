90e174aea915000cf81b13dadedc51ed095a3ff4
v.petrov <mailto.vpv@gmail.com>
2015-11-23 12:04:05 +0300

    a bit more logical version of zbx_sender

--- include/zbx_sender.h
+++ include/zbx_sender.h
@@ -2,15 +2,18 @@
 #define ZBX_L_SENDER_H
 
 #include <string>
+#include <vector>
 #include <unistd.h>
+
+#include "buffer.h"
 
 class tcp_stream
 {
    protected:
       ~tcp_stream() { if (-1 != sd) close(sd); }
 
-      ssize_t send(const void *buffer, size_t len, int flags) const;
-      ssize_t recv(void *buffer, size_t len, int flags) const;
+      ssize_t send(const void *buffer, size_t len, int flags);
+      ssize_t recv(void *buffer, size_t len, int flags);
 
       int sd {-1};
 };
@@ -19,9 +22,9 @@ class tcp_client : private tcp_stream
 {
    public:
       tcp_client(const char *peer_, unsigned port_);
-      
-      ssize_t send(const void *buffer, size_t len, int flags = 0) const;
-      ssize_t recv(void *buffer, size_t len, int flags = 0) const;
+
+      ssize_t send(const void *buffer, size_t len, int flags = 0);
+      ssize_t recv(void *buffer, size_t len, int flags = 0);
 
    private:
       unsigned port;
@@ -30,16 +33,47 @@ class tcp_client : private tcp_stream
       int resolve_hostname(const char *hostname, struct in_addr *addr);
 };
 
+struct sender_data
+{
+   std::string host;
+   std::string key;
+   std::string value;
+   time_t clock;
+};
+
 class zbx_sender : private tcp_client
 {
    public:
-      zbx_sender(const char *peer, unsigned port) : tcp_client{peer, port}, datalen{} { }
-      void send(const void *data, size_t len);
+      zbx_sender(const char *peer, unsigned port) : 
+         tcp_client{peer, port}, datalen{}, tokarr{new json_token[json_arrsize]} { }
+
+      void clear() { data.clear(); }      
+      void send(bool build = true);
+      // In case someone wants to build data on their own.
+      void send(const char *data, size_t len) { databuf.clear(); databuf.mappend(data, len); send(false); }
+
+      template <typename T>
+      void add_data(const std::string &host, const std::string &key, const T &val, time_t clock = 0);
 
    private:
-      uint64_t datalen;
       static const char header[];
       static const size_t data_offset {13};
+      static const size_t json_arrsize {10};
+
+      buffer databuf;
+      uint64_t datalen;
+      std::vector<sender_data> data;
+      std::unique_ptr<json_token []> tokarr;
+
+      void build_data();
 };
+
+template <typename T>
+void zbx_sender::add_data(const std::string &host, const std::string &key, const T &val, time_t clock)
+{
+   std::istringstream ss;
+   ss << val;
+   data.emplace_back(host, key, ss.str(), clock);
+}
 
 #endif
--- src/libs/zbx_sender/zbx_sender.cpp
+++ src/libs/zbx_sender/zbx_sender.cpp
@@ -1,5 +1,6 @@
 #include <stdexcept>
 #include <cstring>
+#include <sstream>
 
 #include <arpa/inet.h>
 #include <netdb.h>
@@ -10,12 +11,12 @@
 
 #include "zbx_sender.h"
 
-ssize_t tcp_stream::send(const void *buffer, size_t len, int flags) const
+ssize_t tcp_stream::send(const void *buffer, size_t len, int flags)
 {
    size_t total = 0;
    const char *ptr = static_cast<const char *>(buffer);
 
-   if (-1 == sd) throw std::runtime_error {"no active connection"};
+   if (-1 == sd) throw std::runtime_error {"no active connection."};
    for (ssize_t n = 0; total < len; total += n)
    {
       n = ::send(sd, ptr + total, len - total, flags);
@@ -24,20 +25,20 @@ ssize_t tcp_stream::send(const void *buffer, size_t len, int flags) const
    return total;
 }
 
-ssize_t tcp_stream::recv(void *buffer, size_t len, int flags) const 
+ssize_t tcp_stream::recv(void *buffer, size_t len, int flags)
 {
    if (-1 == sd) throw std::runtime_error {"no active connection"};
    return ::recv(sd, buffer, len, flags);
 }
 
-ssize_t tcp_client::send(const void *buffer, size_t len, int flags) const
+ssize_t tcp_client::send(const void *buffer, size_t len, int flags)
 {
    ssize_t result = tcp_stream::send(buffer, len, flags);
    if (-1 == result) throw std::runtime_error {std::string{strerror(errno)} + " in send to: " + peer};
    return result;
 }
 
-ssize_t tcp_client::recv(void *buffer, size_t len, int flags) const
+ssize_t tcp_client::recv(void *buffer, size_t len, int flags)
 {
    ssize_t result = tcp_stream::recv(buffer, len, flags);
    if (-1 == result) throw std::runtime_error {std::string{strerror(errno)} + " in recv from: " + peer};
@@ -75,34 +76,68 @@ int tcp_client::resolve_hostname(const char *hostname, struct in_addr *addr)
 
 const char zbx_sender::header[] {'Z', 'B', 'X', 'D', 1};
 
-void zbx_sender::send(const void *data, size_t len)
+void zbx_sender::build_data()
+{
+   bool data_clock {false};
+   databuf.clear();
+   databuf.print(R"**({ "request" : "sender data", "data": [)**");
+
+   for (const auto &entry : data)
+   {
+      if (0 != entry.clock)
+      {
+         databuf.append(R"**({"host":"%s","key":"%s","value":"%s", "clock": %ld},)**", 
+               entry.host.c_str(), entry.key.c_str(), entry.value.c_str(), entry.clock);
+         data_clock = true;
+      }
+
+      else databuf.append(R"**({"host":"%s","key":"%s","value":"%s"},)**", 
+            entry.host.c_str(), entry.key.c_str(), entry.value.c_str());
+   }
+
+   databuf.pop_back();
+   if (data_clock) databuf.append(R"**(], "clock" : %ld})**", time(nullptr));
+   else databuf.append("]}");
+}
+
+void zbx_sender::send(bool build)
 {
    static const char *funcname {"zbx_sender::send"};
    static const char *successfull {"success"};
+   static int suclen = strlen(successfull);
 
-   datalen = len;
+   if (build) 
+   {
+      if (0 == data.size()) return;
+      build_data();
+   }
+
+   datalen = databuf.size();
    tcp_client::send(header, sizeof(header));
    tcp_client::send(&datalen, sizeof(datalen));
-   tcp_client::send(data, len);
+   tcp_client::send(databuf.data(), datalen);
 
-   // NOTE: this is actually a very basic and naive implementation of response reading.
-   // All further code is based on assumption that we will receive all data in one piece,
-   // because response should be small: around 90 bytes data + 13 bytes header.
-   size_t bufsize = 1024;
-   std::unique_ptr<char> buffer {new char[bufsize]};
-   len = tcp_client::recv(buffer.get(), bufsize);
+   size_t offset = data_offset;
+   // Buffer should be at least 1Kb already, which should be enough to store response.
+   size_t len = tcp_client::recv(databuf.mem(), databuf.capacity()); 
 
-   if (len < data_offset) throw logging::error(funcname, "unexpected response length: %lu", len);
-   if (0 != memcmp(header, buffer.get(), sizeof(header)))
-      throw logging::error(funcname, "unexpected header in response");
+   if (sizeof(header) == len)
+   {
+      if (0 != memcmp(header, databuf.data(), sizeof(header)))
+         throw logging::error(funcname, "unexpected header in response");
+      offset -= sizeof(header);
+      len = tcp_client::recv(databuf.mem(), databuf.capacity());
+   }
 
-   json_token *tok;
-   int arrsize = 20;
-   int suclen = strlen(successfull);
+   else
+   {
+      if (len < data_offset) throw logging::error(funcname, "unexpected response length: %lu", len);
+      if (0 != memcmp(header, databuf.data(), sizeof(header)))
+         throw logging::error(funcname, "unexpected header in response");
+   }
 
-   std::unique_ptr<json_token> tokarr {new json_token[arrsize]};
-   parse_json(buffer.get() + data_offset, len - data_offset, tokarr.get(), arrsize);
-
+   json_token *tok;   
+   parse_json(databuf.data() + offset, len - offset, tokarr.get(), json_arrsize);
    if (nullptr == (tok = find_json_token(tokarr.get(), "response")) or
        0 != strncmp(tok->ptr, successfull, tok->len < suclen ? tok->len : suclen))
       throw logging::error(funcname, "unexpected response string: %.*s", tok->len, tok->ptr);
